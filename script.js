d91beaa64d259fac05d488be8008b1de3831a988
IyvonneAyoyi <iyvonneayoyi@gmail.com>
2025-08-03 15:50:04 +0300

    Create addTask function()

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-// Add event listener
+// 1,2 Add event listener and select DOM Elements
 document.addEventListener('DOMContentLoaded', () => {
+    
     const addButton = document.getElementById('add-task-btn');
     const taskInput = document.getElementById('task-input');
     const taskList = document.getElementById('task-list');
+
+// 3. Create the addTask Function:
+    const addTask = () => {
+        // retrieve and trim the value
+        const taskText = taskInput.value.trim();
+
+        // if empty, alert user
+        if (taskText === '') {
+            alert('Please enter a task.');
+            return;
+        }
