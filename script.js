adaf39e756843212493fafabe25d17e876b90637
IyvonneAyoyi <iyvonneayoyi@gmail.com>
2025-08-03 15:40:24 +0300

    Add event listener and select DOM Elements

---
+++ script.js
@@ -0,0 +1,5 @@
+// Add event listener
+document.addEventListener('DOMContentLoaded', () => {
+    const addButton = document.getElementById('add-task-btn');
+    const taskInput = document.getElementById('task-input');
+    const taskList = document.getElementById('task-list');
